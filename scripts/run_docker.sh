a91fd79c1a7401cf9b26d9b58d2da3616a491ea4
影入平羌 <hwenwur@gmail.com>
2021-02-01 20:42:33 +0800

    Feat: check exists container before running

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-docker run -it \
-	--rm \
-	--network=host \
-	--device=/dev/stm32 \
-	--device=/dev/lidar \
-	-v $PWD:/root/SlamRobot \
-	--cap-add=SYS_PTRACE \
-	hwenwur/ros
+function docker_run() {
+	docker run -it \
+		--rm \
+		--network=host \
+		--device=/dev/stm32 \
+		--device=/dev/lidar \
+		-v $PWD:/root/SlamRobot \
+		--cap-add=SYS_PTRACE \
+		hwenwur/ros \
+		bash -c 'cd /root/SlamRobot && bash'
+}
 
+function docker_exec() {
+	local containerId="$1"
+	docker exec -it "$containerId" bash -c 'cd /root/SlamRobot && /ros_entrypoint.sh bash'
+}
+
+function main() {
+	local containerId=$(docker ps | grep hwenwur/ros | awk '{print $1}')
+	if [ -n "$containerId" ]
+	then
+		docker_exec "$containerId"
+	else
+		docker_run
+	fi
+}
+
+set -x
+main
