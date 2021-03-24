45b969b4d7e4ef6e9a8bffc11f90a26ab94c7d01
影入平羌 <hwenwur@gmail.com>
2021-03-24 14:08:52 +0800

    Add VelocityRedeader testcase

---
+++ test/velocity_feedback/main.cpp
@@ -0,0 +1,23 @@
+#include "VelocityReader.h"
+#include "robot_serial.h"
+#include <iostream>
+#include <unistd.h>
+
+
+int main()
+{
+  robotserial::Serial mSerial("/dev/ttyUSB0", 115200);
+  VelocityReader reader(mSerial);
+  reader.startReadLoop();
+  SerialFrameTimestamped vel;
+  bool r;
+  while(true)
+  {
+    r = reader.lookupLatestFrame(vel);
+    if(r)
+      printf("%.2f, %.2f, %.2f, %lu\n", vel.frame.vx, vel.frame.vy, vel.frame.omega, vel.timestamp / 1000);
+    sleep(1);
+  }
+  reader.stopReadLoop();
+  return 0;
+}
